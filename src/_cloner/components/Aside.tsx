711428eaf81dda30f244595f6cc5c6661c4310c4
iamabolfazlmasoumi <iamabolfazlmasoumi@gmail.com>
2023-06-27 13:08:06 +0330

    aside li

--- src/_cloner/components/Aside.tsx
+++ src/_cloner/components/Aside.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { KTSVG } from "./KTSvg";
 
 interface IProps {
     isVisible: boolean;
@@ -10,9 +11,44 @@ const Aside: FC<IProps> = ({ isVisible }) => {
                 isVisible ? "flex" : "hidden"
             } w-96 md:bottom-10 md:right-10 md:top-10 md:flex`}
         >
-            <div className="flex flex-grow flex-col dark:bg-slate-800 bg-indigo-800 md:rounded-3xl">
+            <div className="flex flex-grow flex-col bg-indigo-800 dark:bg-slate-800 md:rounded-3xl">
                 <div className="p-12 font-yekan_bold text-2xl text-white">
                     مدیریت امور مشتریان
+                </div>
+                <div>
+                    <div className="flex gap-2 px-12 py-4">
+                        <div className="logo">
+                            <KTSVG
+                                path={"/media/logos/arr001.svg"}
+                                svgClassName="h-[18px]"
+                                className="text-white"
+                            />
+                        </div>
+                        <div className="font-yekan_bold text-xl text-white">
+                            داشبورد
+                        </div>
+                    </div>
+                    <div className="flex gap-2 px-12 py-4">
+                        <div className="logo">
+                            <KTSVG
+                                path={"/media/logos/arr001.svg"}
+                                svgClassName="h-[18px]"
+                                className="text-white"
+                            />
+                        </div>
+                        <div className="font-yekan_bold text-xl text-white">
+                            مدیریت کاربران
+                            <ul className="py-4">
+                                <li className="py-2 text-white text-opacity-60">
+                                    <span className="w-[50px] h-[50px] bg-white"></span>
+                                    افزودن کاربر
+                                </li>
+                                <li className="py-2 text-white text-opacity-60">
+                                    ویرایش کاربر
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </aside>
